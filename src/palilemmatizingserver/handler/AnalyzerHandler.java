64628dac19a62550ffc96479445489e03adc1628
David Alfter <blueygh2@gmail.com>
2014-09-18 15:47:01 +0200

    Fixed method calls.

--- src/palilemmatizingserver/handler/AnalyzerHandler.java
+++ src/palilemmatizingserver/handler/AnalyzerHandler.java
@@ -20,7 +20,7 @@ public class AnalyzerHandler extends AbstractHandler
 	@Override
 	public ResponseContainer processRequest(AppRuntime ar,
 			ClientRequest request, ILogInterface log) throws Exception
-	{
+			{
 		log.info("Invoking analyzer");
 
 		// ----------------------------------------------------------------
@@ -28,52 +28,49 @@ public class AnalyzerHandler extends AbstractHandler
 
 		String word = request.getRequestParameter("word");
 		JObject gramGrp = this.getParamJObject(request, "gramGrp");
-		
+
 		if (gramGrp == null) {
 			List<JObject> gramGrps = this.getGramGrpFromDictionary(word, "LEMMA", ar, log);
 			if (gramGrps == null) {
 				log.warn("No grammar node found for " + word);
 			}
 			//	return ResponseContainer.createJSONResponse(EnumError.PROCESSING, WordConverter.toJObject("{}"));
-			
+
 			// Premature return block
 			else if (gramGrps.size() > 1) {
 				List<ConstructedWord> prematureResult = new ArrayList<ConstructedWord>();
 				for (JObject innerGramGrp : gramGrps) {
-					
-					
-					List<ConstructedWord> words = ar.getMorphologyAnalyzer().analyze(log, word);
-					
-					
+
+
+					List<ConstructedWord> words = ar.getMorphologyAnalyzer().analyzeWithOptions(log, word);
+
+
 					prematureResult.addAll(words);
 				}
 				JObject ret = (JObject)(ar.getFormatConverterManager().convert("generatedwordforms", "json", prematureResult, null));
-				
+
 				return ResponseContainer.createJSONResponse(ret);
 			}
 			// End premature return block
-			
+
 		} else {
 			gramGrp = WordConverter.toJObject("{gramGrp:" +
 					gramGrp.toJSON() + "}");
 		}
 		// ----------------------------------------------------------------
 		// analyze
-		
+
 		String json;
 		MorphologyAnalyzer ma = ar.getMorphologyAnalyzer();
 		try {
-			if (gramGrp == null)
-				json = ma.analyzeWithDictionary(log, word);
-			else
-				json = ma.analyzeWithDictionary(log, word, gramGrp);
+			json = ma.analyzeWithDictionary(log, word, gramGrp);
 		} catch (Exception e) {
 			return createError(e);
 		}
-		
+
 		// ----------------------------------------------------------------
 		// create response
 		return createSuccessResponse(WordConverter.toJObject(json));
-	}
+			}
 
 }
